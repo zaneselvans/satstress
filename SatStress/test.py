26a26586fa4d2279f6d8532602325bfeb3b360c3
Zane Selvans <zane.selvans@gmail.com>
2008-04-02 01:23:25 +0000

    Committing so I can move it

--- SatStress/test.py
+++ SatStress/test.py
@@ -13,48 +13,57 @@ test}.  It also acts as a short demonstration of how to use the SatStress
 package.
 
 """
-
 import sys
+import os
 import pickle
 import scipy
 import SatStress as SS
 
-# Create a new satellite object, as defined by the input file:
-the_sat = SS.Satellite(open('input/Europa.satellite','r'))
+def main():
+    package_dir    = os.path.dirname(os.path.abspath(__file__))
+    test_outfile   = os.path.join(package_dir, "test", "test_nsr_diurnal.pkl")
+    test_satellite = os.path.join(package_dir, "input", "Europa.satellite")
 
-# Create a StressCalc object, that calculates both the NSR and Diurnal
-# stresses on the Satellite just instantiated:
-the_stresses = SS.StressCalc([SS.Diurnal(the_sat), SS.NSR(the_sat)])
+    # Create a new satellite object, as defined by the input file:
+    the_sat = SS.Satellite(open(test_satellite,'r'))
 
-# do a series of calculations, varying all the inputs at each iteration
-theta = 0.0
-phi   = 0.0
-t     = 0.0
-
-Taulist = []
-
-while the_stresses.stresses[1].Delta() > 0.01 and t < 2.0*scipy.pi/the_sat.mean_motion():
-    print("""Tau(theta = %g, phi = %g, time = %g, Delta = %g) = """ % (scipy.degrees(theta), scipy.degrees(phi), t, the_stresses.stresses[1].Delta()))
-    Taulist.append(the_stresses.tensor(theta=theta, phi=phi, t=0))
-    print Taulist[-1], "\n"
-    theta = theta + scipy.pi/23.0
-    phi   = phi   + scipy.pi/11.0
-    t     = t     + (scipy.pi/11.0)/the_sat.mean_motion()
-
-    # Re-construct the NSR stress (and the_stresses StressCalc object)
-    # changing the forcing period, so that we can have the NSR stresses
-    # vary with each iteration too.
-    the_sat.nsr_period = the_sat.nsr_period / 1.5
+    # Create a StressCalc object, that calculates both the NSR and Diurnal
+    # stresses on the Satellite just instantiated:
     the_stresses = SS.StressCalc([SS.Diurnal(the_sat), SS.NSR(the_sat)])
 
-# Now we want to compare this against reference output, just to make sure
-# that we're getting the right numbers...
+    # do a series of calculations, varying all the inputs at each iteration
+    theta = 0.0
+    phi   = 0.0
+    t     = 0.0
 
-pickledTau = pickle.load(open('test/SS_test_calc.pkl', 'r'))
-for (tau1, tau2) in zip(Taulist, pickledTau):
-    if tau1.all() != tau2.all():
-        print("\nTest failed.  :(\n")
-        sys.exit(1)
+    # a list to store the calculation results in for later checking
+    Taulist = []
 
-print("\nTest passed! :)\n")
-sys.exit(0)
+    while the_stresses.stresses[1].Delta() > 0.01 and t < 2.0*scipy.pi/the_sat.mean_motion():
+        print("""Tau(theta = %g, phi = %g, time = %g, Delta = %g) = """ % (scipy.degrees(theta), scipy.degrees(phi), t, the_stresses.stresses[1].Delta()))
+        Taulist.append(the_stresses.tensor(theta=theta, phi=phi, t=0))
+        print Taulist[-1], "\n"
+        theta = theta + scipy.pi/23.0
+        phi   = phi   + scipy.pi/11.0
+        t     = t     + (scipy.pi/11.0)/the_sat.mean_motion()
+
+        # Re-construct the NSR stress (and the_stresses StressCalc object)
+        # changing the forcing period, so that we can have the NSR stresses
+        # vary with each iteration too.
+        the_sat.nsr_period = the_sat.nsr_period / 1.5
+        the_stresses = SS.StressCalc([SS.Diurnal(the_sat), SS.NSR(the_sat)])
+
+    # Now we want to compare this against reference output, just to make sure
+    # that we're getting the right numbers...
+
+    pickledTau = pickle.load(open(test_outfile, 'r'))
+    for (tau1, tau2) in zip(Taulist, pickledTau):
+        if tau1.all() != tau2.all():
+            print("\nTest failed.  :(\n")
+            sys.exit(1)
+
+    print("\nTest passed! :)\n")
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
