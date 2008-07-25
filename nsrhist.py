c897fe9f510b640a54dc1c0522949686e3017a11
Zane Selvans <zane.selvans@gmail.com>
2008-07-25 07:54:55 +0000

    Re-organized logic out of this script and into the satstress/lineament class separated drawing and calculation functions into different functions

--- nsrhist.py
+++ nsrhist.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 """
-A script to compare a set of lineaments to a given NSR stress field.
+Routines for comparing a set of lineaments to an NSR stress field.
 
 """
 
 from satstress import *
 from pylab import *
 
-def calc_nsr_fit_hist(satfile = "input/ConvectingEuropa_GlobalDiurnalNSR.ssrun",\
-                      shapefile = "input/GlobalLineaments",\
-                      nb = 180,\
-                      numlins = "all"):
+def calc_nsr_fits(satfile = "input/ConvectingEuropa_GlobalDiurnalNSR.ssrun",\
+                  shapefile = "input/GlobalLineaments",\
+                  nb = 180,\
+                  numlins = "all"): #{{{
     
     # By default do all the lineaments... but allow us to shorten for testing
     if numlins != "all":
@@ -35,30 +35,49 @@ def calc_nsr_fit_hist(satfile = "input/ConvectingEuropa_GlobalDiurnalNSR.ssrun",
     for lin in lins:
         print "lineament %d (%d segments, %g km)" % (lin_num, len(lin.vertices), lin.length()*sat_radius_km)
         lin.calc_fits(nsr, nb)
+        lin.sat_radius_km = sat_radius_km
         lin_num += 1
 
+    return (lins)
+# }}}
+
+def calc_fit_hist(lins): #{{{
+    linhist = []
+    for lin in lins:
         # We generate the histogram dataset, adding one "count" for each km of
         # lineament that fit best at that value of b(ackrotation)
-        linhist += int(lin.length()*sat_radius_km)*[degrees(lin.best_b()),]
+        linhist += int(lin.length()*lin.sat_radius_km)*[degrees(lin.best_b()),]
 
-    drawnsrhist(histdata=linhist, radius=sat_radius_km)
+    return(linhist)
 
-    # Output the shapefile with its new attributes
-    # TODO
-
-    return (lins, linhist, sat_radius_km)
-
-    axis([0,180,0,90])
-    plot(degrees(backrots), degrees(lindict['fits']))
-    grid(True)
+def draw_fit_hist(linhist, bins=18):
+    hist(linhist, bins=bins)
+    axis(xmin=0, xmax=180)
+    xlabel("degrees of backrotation")
+    ylabel("km of lineaments")
+    title("Lineament formation vs. best fit backrotation")
     show()
 # }}}
 
-def draw_nsr_fit_hist(linhist, bins=18): #{{{
-     hist(linhist, bins=bins)
-     axis(xmin=0, xmax=180)
-     xlabel("degrees of backrotation")
-     ylabel("km of lineaments")
-     title("Lineament formation vs. best fit backrotation")
-     show()
- # }}}
+def draw_fits(lins): #{{{
+    lin_num=0
+    for lin in lins:
+        # Plot the lineament itself:
+        lons = [ v[0] for v in lin.vertices ]
+        lats = [ v[1] for v in lin.vertices ]
+        plot(mod(degrees(lons),180), abs(degrees(lats)), 'k-', linewidth=2)
+        plot(linspace(0,180,num=len(lin.fits)), degrees(lin.fits))
+        plot(linspace(0,180,10), 10*[degrees(lin.best_fit()),], 'r--')
+        plot(10*[degrees(lin.best_b()),], linspace(0,90,10), 'r--')
+        #grid(True)
+        axis([0,180,0,91])
+        xticks( range(0,181,15) )
+        yticks( range(0,91,15) )
+        xlabel("degrees of backrotation")
+        ylabel("average misfit (degrees)")
+        title("lin=%d, length=%gkm, best_fit=%g, best_b=%g, iqr=%g" % (lin_num, lin.sat_radius_km*lin.length(), degrees(lin.best_fit()), degrees(lin.best_b()), degrees(lin.fits_width()) ))
+        show()
+        x = raw_input("press return for next fit: ")
+        clf()
+        lin_num += 1
+# }}}
